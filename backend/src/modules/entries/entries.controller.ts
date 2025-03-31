60cd6bfc58882848408eec9587983e2cf7945f9e
Patrik Hoferica <p.hoferica@outlook.com>
2025-03-31 14:15:57 +0200

    handle entries

--- backend/src/modules/entries/entries.controller.ts
+++ backend/src/modules/entries/entries.controller.ts
@@ -7,11 +7,14 @@ import {
   Put,
   Delete,
   HttpCode,
+  UseGuards,
 } from '@nestjs/common';
 import { EntriesService } from './entries.service';
 import { CreateEntryDto } from './dto/create-entry.dto';
 import { Entry } from 'src/entities/entry.entity';
 import { Category } from 'src/entities/category.entity';
+import { UserId } from '../auth/decorators/user-id.decorator';
+import { PremiumUserGuard } from '../auth/guards/premium-user.guard';
 
 @Controller('entries')
 export class EntriesController {
@@ -19,37 +22,55 @@ export class EntriesController {
 
   @Post()
   @HttpCode(201)
-  async create(@Body() entryDto: CreateEntryDto): Promise<Entry> {
-    return await this.entriesService.create(entryDto);
+  async create(
+    @Body() entryDto: CreateEntryDto,
+    @UserId() userId: number,
+  ): Promise<Entry> {
+    return await this.entriesService.create(entryDto, userId);
   }
 
   @Get()
   @HttpCode(200)
-  async findAll(): Promise<Entry[]> {
-    console.log('categories');
-    return await this.entriesService.findAll();
+  async findAll(@UserId() userId: number): Promise<Entry[]> {
+    return await this.entriesService.findAll(userId);
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  async findOne(
+    @Param('id') id: number,
+    @UserId() userId: number,
+  ): Promise<Entry> {
+    return await this.entriesService.findOne(id, userId);
   }
 
   @Get('category/:categoryId')
   @HttpCode(200)
   async getEntriesByCategory(
     @Param('categoryId') id: number,
+    @UserId() userId: number,
   ): Promise<{ category: Category; entries: Entry[] }> {
-    return await this.entriesService.getEntriesByCategory(id);
+    return await this.entriesService.getEntriesByCategory(id, userId);
   }
 
   @Put(':id')
   @HttpCode(200)
+  @UseGuards(PremiumUserGuard)
   async update(
     @Param('id') id: number,
     @Body() updateEntryDto: Partial<CreateEntryDto>,
+    @UserId() userId: number,
   ): Promise<Entry> {
-    return await this.entriesService.update(id, updateEntryDto);
+    return await this.entriesService.update(id, updateEntryDto, userId);
   }
 
   @Delete(':id')
   @HttpCode(204)
-  async remove(@Param('id') id: number): Promise<void> {
-    await this.entriesService.remove(id);
+  @UseGuards(PremiumUserGuard)
+  async remove(
+    @Param('id') id: number,
+    @UserId() userId: number,
+  ): Promise<void> {
+    await this.entriesService.remove(id, userId);
   }
 }
