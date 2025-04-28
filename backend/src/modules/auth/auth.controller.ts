0d9f7f7e3265ed45e99b1f7ffa15523176ce1206
Patrik Hoferica <p.hoferica@outlook.com>
2025-04-28 22:15:45 +0200

    swagger tags example

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -3,23 +3,33 @@ import { AuthService } from './auth.service';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserSignupDto } from './dto/user-signup.dto';
 import { Role } from 'src/utils/enums';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'User login' })
+  @ApiResponse({ status: 200, description: 'Login successful' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   async login(@Body() loginDto: UserLoginDto) {
     return this.authService.login(loginDto);
   }
 
   @Post('signup')
+  @ApiOperation({ summary: 'Register new user' })
+  @ApiResponse({ status: 201, description: 'User successfully created' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   signup(@Body() signUpDto: UserSignupDto) {
     signUpDto.role = Role.User;
     return this.authService.signup(signUpDto);
   }
 
   @Post('create-admin')
+  @ApiOperation({ summary: 'Create admin user' })
+  @ApiResponse({ status: 201, description: 'Admin user successfully created' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   createAdmin(@Body() signUpDto: UserSignupDto) {
     signUpDto.role = Role.Admin;
     return this.authService.signup(signUpDto);
